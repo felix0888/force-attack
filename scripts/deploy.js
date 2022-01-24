c5109f13079b6c01861f303b81293458183ae73a
felix0888 <felixnan88@gmail.com>
2022-01-24 18:44:35 -0500

    init hardhat project, Force contract done

---
+++ scripts/deploy.js
@@ -0,0 +1,24 @@
+const hre = require("hardhat");
+const { ethers } = require("hardhat");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log(
+    "Deploying contracts with the account: ", deployer.address
+  );
+
+  console.log("Account balance: ", (await deployer.getBalance()).toString());
+
+  const Force = await ethers.getContractFactory("Force");
+  const force = await Force.deploy();
+  console.log("Force address: ", await force.address);
+  console.log("Account balance after Force deploy: ", (await deployer.getBalance()).toString());
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
